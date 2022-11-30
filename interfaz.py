2dfcff98a95f8128cd77229d5a2e564e43b56115
Danny Loor <danny_loor00@hotmail.com>
2022-11-29 19:16:12 -0500

    interfaz inicial

---
+++ interfaz.py
@@ -0,0 +1,56 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import QKeySequence
+from PyQt5 import Qt
+
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        # Acciones
+        toolbar_herramientas = QToolBar()
+        self.addToolBar(toolbar_herramientas)
+        
+        action_lexico =  QAction("Análisis Léxico", self)
+        action_sintactico =  QAction("Análisis Sintácico", self)
+        action_semantico =  QAction("Análisis Semántico", self)
+        action_limpiar =  QAction("Limpiar todo", self)
+        toolbar_herramientas.addAction(action_lexico)
+        toolbar_herramientas.addAction(action_sintactico)
+        toolbar_herramientas.addAction(action_semantico)
+        toolbar_herramientas.addSeparator()
+        toolbar_herramientas.addAction(action_limpiar)
+
+        # Editor de código
+        self.label_input = QLabel("Editor")
+        self.input = QPlainTextEdit()
+
+        # Salida análisis
+        self.label_output = QLabel("Salida Análisis")
+        self.output = QPlainTextEdit()
+        self.output.setReadOnly(True)
+
+        # Contenedor principal
+        self.layout = QHBoxLayout()
+        grid = QVBoxLayout()
+
+        grid.addWidget(self.label_input)
+        grid.addWidget(self.input)
+
+        grid.addWidget(self.label_output)
+        grid.addWidget(self.output)
+
+        self.layout.addLayout(grid)
+        self.contenedor = QWidget()
+        self.setCentralWidget(self.contenedor)
+        self.contenedor.setLayout(self.layout)
+
+        self.show()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    app.setApplicationName("Analizador de código GO")
+    w = Window()
+    w.showMaximized()
+    sys.exit(app.exec_())
+    #app.exec_()
